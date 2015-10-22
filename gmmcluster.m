0f3d2813fc20a76b8b2f49520ba9d6f895990683
Shubhojyoti <gangulyshubhojyoti@gmail.com>
2015-10-22 22:43:23 +0530

    Fixed gmm mixture generation for lower matlab versions

--- gmmcluster.m
+++ gmmcluster.m
@@ -1,6 +1,7 @@
 function [obj,idx]=gmmcluster(I,K)
 
-I=im2double(imread((I)));
+[dimx,dimy] = size(I);
+I=im2double(I);
 %img is the name of the image
 %k is the no of clusters
 %read file
@@ -14,20 +15,28 @@ I = 255 * uint8(I);
 
  [cInd, c] = kmeans([datax datay], K, 'EmptyAction','singleton',...
     'maxiter',1000,'start','cluster');
-obj = fitgmdist(I,K,'Regularizationvalue',0.1);
-idx = cluster(obj,I);
-cluster1 = I(idx == 1,:);
-cluster2 = I(idx == 2,:);
 
- figure;
+ %============================================================
+ %Generate prior GSM here and feed it as a starting point of EM
+ %============================================================
+%For newer versions of MATLAB, uncomment below:
+%obj = fitgmdist(I,K,'Regularizationvalue',0.1);
+
+options = statset('MaxIter',500);
+obj = gmdistribution.fit([datax, datay],K,'Regularize',0.1,'Options',options);
+
+
+figure;
  
- imshow(I);
- gscatter(datay,-datax);
-hold on
-h1=scatter(cluster1(:,1),cluster1(:,2),10,'r.');
-h2=scatter(cluster2(:,1),cluster2(:,2),10,'g.');
+ezsurf(@(x,y)pdf(obj,[x y]),[0 dimx],[0 dimy]);
 
-% scatter(obj.mu(:,2),-obj.mu(:,1),20,'ko'); %same funky coordinates
- scatter(c(:,2),-c(:,1),20,'k+'); %same funky coordinates
- legend([h1 h2],'Cluster 1','Cluster 2','Location','NW')
+%imshow(I);
+%  gscatter(datay,-datax);
+% hold on
+% h1=scatter(cluster1(:,1),cluster1(:,2),10,'r.');
+% h2=scatter(cluster2(:,1),cluster2(:,2),10,'g.');
+% 
+% % scatter(obj.mu(:,2),-obj.mu(:,1),20,'ko'); %same funky coordinates
+%  scatter(c(:,2),-c(:,1),20,'k+'); %same funky coordinates
+%  legend([h1 h2],'Cluster 1','Cluster 2','Location','NW')
 end
